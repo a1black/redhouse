6fd0042f54e4abdbccc3b41811fc1c46f8bbdf06
a1black <37011601+a1black@users.noreply.github.com>
2018-10-16 13:53:30 +0300

    Initial commit.

---
+++ themes/website/assets/src/js/app.js
@@ -0,0 +1,11 @@
+global.jQuery = require('jquery')
+var $ = global.jQuery
+window.$ = $
+
+$(function() {
+    "use strict";
+    // Your js script is below this line
+    // --------------------------------------------------------------------- //
+
+    console.log( "ready!" );
+});
