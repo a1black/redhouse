0e8247afb0710180c768ff22ba3bed7481722259
a1black <37011601+a1black@users.noreply.github.com>
2018-10-20 19:49:52 +0300

    Work in progress.

--- themes/website/assets/src/js/app.js
+++ themes/website/assets/src/js/app.js
@@ -7,5 +7,23 @@ $(function() {
     // Your js script is below this line
     // --------------------------------------------------------------------- //
 
-    console.log( "ready!" );
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 56
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function() {
+        if ($("#mainNav").offset().top > 200) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+
 });
